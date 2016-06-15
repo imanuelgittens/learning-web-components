14cb9613052d02a0376f178f12b7818256e33fda
Imanuel Gittens <gittensimanuel@gmail.com>
2016-06-15 16:23:24 -0400

    Start working on validations for forms.

---
+++ components/forms/login/login.js
@@ -0,0 +1,25 @@
+var email = document.getElementById('email');
+var password = document.getElementById('password');
+
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+
+
+window.onload = function() {
+
+  email.addEventListener('keyup', function(event) {
+    if (!validator.isEmailAddress(email.value)) {
+      email.setCustomValidity("I expect an e-mail, darling!");
+    } else {
+      email.setCustomValidity("");
+    }
+  });
+
+}
+
+
+
+
