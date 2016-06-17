545f0d5739a9c8358263fa31573eb274455d755a
Imanuel Gittens <gittensimanuel@gmail.com>
2016-06-16 21:15:21 -0400

    Update listener for constraint API function.

--- components/forms/login/login.js
+++ components/forms/login/login.js
@@ -1,16 +1,17 @@
+
 var email = document.getElementById('email');
 var password = document.getElementById('password');
 
-function validateEmail(email) {
+/*function validateEmail(email) {
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
-}
+}*/
 
 
 
 window.onload = function() {
 
-  email.addEventListener('keyup', function(event) {
+  email.addEventListener('input', function(event) {
     if (!validator.isEmailAddress(email.value)) {
       email.setCustomValidity("I expect an e-mail, darling!");
     } else {
