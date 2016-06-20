c07639dbd7211e4913eb6d5182b970480b0b11f0
Imanuel Gittens <gittensimanuel@gmail.com>
2016-06-20 19:21:51 -0400

    Start product js functions.

--- components/products/products.js
+++ components/products/products.js
@@ -1,14 +1,76 @@
 window.addEventListener('load', function(){
 
-	var isActive = document.querySelector(".inactive");
-	var toggleButton = document.querySelector('.toogle-cart');
+	var productList = [{
+	    id: 1,
+	    name: 'Pen',
+	    price: 2,
+	    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+	},{
+	    id: 2,
+	    name: 'Pencil',
+	    price: 3,
+	    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elitre ma.',
+	},{
+	    id: 3,
+	    name: 'Notebook',
+	    price: 5,
+	    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,  mag.',
+	},{
+	    id: 4,
+	    name: 'Ruler',
+	    price: 1,
+	    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, cii.',
+	}];
+
+	
+	var toggleButton = document.querySelector('.toggle-cart');
+
+	function toggleCart(){
+		var cart = document.getElementById('shoppingCart');
+		if(cart){
+			if(cart.className === 'inactive'){
+				document.getElementById('shoppingCart').className = "active";
+			}else{
+				document.getElementById('shoppingCart').className = "inactive";
+			}
+		}
+	}
+
+/*	function loadProducts(products){
+		var i,product, image, shortDesc, price, addToCart;
+		var productList = document.getElementById("productList");
+		//var product = '';
+		for(i = 0; i < products.length; i++){
+			product = document.createElement('div');
+			product.className = "quarter product";
+
+			image = document.createElement('div');
+			image.className = "featured-img";
+
+			shortDesc = document.createElement('div');
+			shortDesc.className = "short-desc";
+
+			price = document.createElement('div');
+			price.className = "price";
+
+			addToCart = document.createElement('div');
+			addToCart.className = "add-to-cart";
+
+
+
+
+		}
+	}*/
+
+/*	loadProducts(productList);*/
+
+	//show/hide cart
 
 	toggleButton.addEventListener('click', function(event){
-		if(isActive){
-			
-		}
+		toggleCart();
 	});
 
-	console.log(isActive);
+	//end show/hide cart
+
 
 });
