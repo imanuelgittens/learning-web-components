1e1f30fb7f415601e442342af7c3e13014297dd0
Imanuel Gittens <gittensimanuel@gmail.com>
2016-06-22 12:57:46 -0400

    Continue working on cart functionality.

--- components/products/products.js
+++ components/products/products.js
@@ -1,68 +1,131 @@
 window.addEventListener('load', function(){
 
 	var productList = [{
-	    id: 1,
+	    id: 'product1',
 	    name: 'Pen',
 	    price: 2,
 	    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
 	},{
-	    id: 2,
+	    id: 'product2',
 	    name: 'Pencil',
 	    price: 3,
 	    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elitre ma.',
 	},{
-	    id: 3,
+	    id: 'product3',
 	    name: 'Notebook',
 	    price: 5,
 	    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,  mag.',
 	},{
-	    id: 4,
+	    id: 'product4',
 	    name: 'Ruler',
 	    price: 1,
 	    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, cii.',
 	}];
 
-	
+	var shoppingCart = [];
+
 	var toggleButton = document.querySelector('.toggle-cart');
+	var addToCartButton = document.querySelector('.cartAdd');
+
+
+	function showCart(){
+		document.getElementById('shoppingCart').className = "active";
+	}
+
+	function hideCart(){
+		document.getElementById('shoppingCart').className = "inactive";
+	}
 
 	function toggleCart(){
 		var cart = document.getElementById('shoppingCart');
 		if(cart){
 			if(cart.className === 'inactive'){
-				document.getElementById('shoppingCart').className = "active";
+				showCart();
 			}else{
-				document.getElementById('shoppingCart').className = "inactive";
+				hideCart();
 			}
 		}
 	}
 
-/*	function loadProducts(products){
-		var i,product, image, shortDesc, price, addToCart;
-		var productList = document.getElementById("productList");
-		//var product = '';
-		for(i = 0; i < products.length; i++){
-			product = document.createElement('div');
-			product.className = "quarter product";
+	function buildCartItems(){
+		var i, lineItem, prodString;
+		var cartDetails = document.querySelector('.cart-products');
+		cartDetails.innerHTML = "";
 
-			image = document.createElement('div');
-			image.className = "featured-img";
+		for(i = 0; i < shoppingCart.length; i++){
+			
+			lineItem = document.createElement('div');
 
-			shortDesc = document.createElement('div');
-			shortDesc.className = "short-desc";
+			prodString = '<div class="sixth cart-options">';
+			prodString += '<button class="remove-link" id="remove-product'+i+'">Remove</button>';
+			prodString += '</div>';
+			prodString += '<div class="sixth cart-item">';
+			prodString += '<img src="http://placehold.it/70x70">';
+			prodString += '</div>';
+			prodString += '<div class="third cart-desc">';
+			prodString += '<h3>'+shoppingCart[i].name+'</h3>';
+			prodString += '<p>'+shoppingCart[i].desc+'</p>';
+			prodString += '</div>';
+			prodString += '<div class="sixth cart-qty">';
+			prodString += '<input class="prod-qty" type="text" value="'+shoppingCart[i].quantity+'" id="update-product'+i+'"/>';
+			prodString += '</div>';
+			prodString += '<div class="sixth cart-price">';
+			prodString += '<h4><strong>$'+shoppingCart[i].price+'</strong></h4>';
+			prodString += '</div>';
 
-			price = document.createElement('div');
-			price.className = "price";
-
-			addToCart = document.createElement('div');
-			addToCart.className = "add-to-cart";
-
-
-
-
+			lineItem.innerHTML = prodString;
+			cartDetails.appendChild(lineItem);
 		}
-	}*/
+	}
 
-/*	loadProducts(productList);*/
+	function addToCart(product){
+		var singleProduct = {};
+		singleProduct.id = product.id;
+		singleProduct.name = product.name;
+		singleProduct.desc = product.desc;
+		singleProduct.price = product.price;
+		singleProduct.quantity = 1;
+		shoppingCart.push(singleProduct);
+
+		buildCartItems();
+		totalPrice();
+		showCart();
+	}
+
+	function removeFromCart(index){
+		shoppingCart.splice(index, 1);
+		buildCartItems();
+		totalPrice();
+		if(shoppingCart.length  <= 0){
+			hideCart();
+		}
+	}
+
+	function changeQuantity(index, value){
+		shoppingCart[index].quantity = value;
+		totalPrice();
+	}
+//to finish
+	function totalPrice(){
+		var subTotal = document.getElementById('subTotal');
+		var shippingCost = document.getElementById('shippingCost');
+		var total = document.getElementById('total');
+//		console.log(subTotal);
+		var i;
+		var result = 0;
+		for(i = 0; i < shoppingCart.length; i++){
+			result = result + (shoppingCart[i].price * shoppingCart[i].quantity);
+		}
+		subTotal.innerHTML = '$' + result;
+//console.log(result);
+
+	}
+
+	function promoCode(){
+
+	}
+
+
 
 	//show/hide cart
 
@@ -70,7 +133,78 @@ window.addEventListener('load', function(){
 		toggleCart();
 	});
 
+
 	//end show/hide cart
 
+//----
+
+	if(document.addEventListener){
+		document.addEventListener('click', handleAddCartClick, false);
+	}else{
+		if(document.attachEvent){
+			document.attachEvent('onclick', handleAddCartClick);
+		}
+	}
+
+	function handleAddCartClick(event){
+		event = event || window.event;
+		event.target = event.target || event.srcElement;
+		var element = event.target;
+
+		var productNumber;
+
+		if(element.className === 'cartAdd'){
+			productNumber = element.id.charAt(element.id.length-1);
+			addToCart(productList[Number(productNumber)]);
+		}
+	}
+//----
+	if(document.addEventListener){
+		document.addEventListener('click', handleRemoveCartClick, false);
+	}else{
+		if(document.attachEvent){
+			document.attachEvent('onclick', handleRemoveCartClick);
+		}
+	}
+
+	function handleRemoveCartClick(event){
+		event = event || window.event;
+		event.target = event.target || event.srcElement;
+		var element = event.target;
+
+		var productNumber;
+
+		if(element.className === 'remove-link'){
+			productNumber = element.id.charAt(element.id.length-1);
+			removeFromCart(productNumber);
+
+		}
+	}
+//---
+	if(document.addEventListener){
+		document.addEventListener('blur', handleQuantityChangeBlur, true);
+	}else{
+		if(document.attachEvent){
+			document.attachEvent('onblur', handleQuantityChangeBlur);
+		}
+	}
+
+	function handleQuantityChangeBlur(event){
+		event = event || window.event;
+		event.target = event.target || event.srcElement;
+		var element = event.target;
+
+		var productNumber, value;
+
+		if(element.className === 'prod-qty'){
+			value = element.value;
+			productNumber = element.id.charAt(element.id.length-1);
+			if(value <= 0){
+				removeFromCart(productNumber);
+			}else{
+				changeQuantity(productNumber, value);
+			}
+		}
+	}
 
 });
