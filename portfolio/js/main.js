93ec440d51459867fa19992c42bbb69daa448bb5
Imanuel Gittens <gittensimanuel@gmail.com>
2017-01-03 16:22:14 -0400

    Restart logic for portfolio.

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -3,5 +3,8 @@
 var home = document.querySelector('#home-block');
 
 home.addEventListener('click', function(){
-	this.classList.add('portfolio__row__block--loading');
+	var content = document.getElementsByClassName('content');
+	var homeContent = document.getElementById('homeContent-block');
+	content[0].classList.add('content--show')
+	homeContent.classList.add('content__item--show');
 });
