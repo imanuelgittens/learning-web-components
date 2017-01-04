d012dcbaab33e2eb42aa82066a67ffcc122c4350
Imanuel Gittens <gittensimanuel@gmail.com>
2017-01-04 15:57:01 -0400

    Start working on dummy expanding element.

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -3,7 +3,6 @@
 	var gridItemsContainer = document.querySelector('section.grid');
 	var gridItems = gridItemsContainer.querySelectorAll('.grid__item');
 	var currentItem = -1;
-	console.log(gridItemsContainer);
 
 	function init(){
 		initEvents();
@@ -18,9 +17,36 @@
 
 				//simulate loading time..
 				item.classList.add('grid-item--loading');
+				setTimeout(function(){
+					/*item.classList.add('grid-item--animate');*/
+					loadContent(item);
+				},3000);
+				
 
 			});
 		});
+	}
+
+	function loadContent(item){
+		// add expanding element/placeholder 
+		var dummy = document.createElement('div');
+		//get position on the screen as well as the width and height of the grid item
+		var itemDimensionsPosition = item.getBoundingClientRect();
+		
+
+		dummy.className = 'placeholder';
+		dummy.classList.add('placeholder--trans-in');
+		dummy.style.top = itemDimensionsPosition.top + 'px';
+		dummy.style.left = itemDimensionsPosition.left + 'px';
+		dummy.style.width = itemDimensionsPosition.width + 'px';
+		dummy.style.height = itemDimensionsPosition.height + 'px';
+		dummy.style.position = 'absolute';
+		dummy.style.backgroundColor = "#fff";
+		console.log(item.style.backgroundColor);
+
+		gridItemsContainer.appendChild(dummy);
+
+		
 	}
 
 	init();
