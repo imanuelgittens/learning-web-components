734fdb3540d7788fb7878fe0138e214625da231a
Imanuel Gittens <gittensimanuel@gmail.com>
2017-06-18 12:25:47 -0400

    Update portfolio to work with handlebarsjs.

--- masteringCSS/projectAssignment5/scripts/randomNumberFact.js
+++ masteringCSS/projectAssignment5/scripts/randomNumberFact.js
@@ -1,24 +1,26 @@
-'use strict';
+(function(){
+  'use strict';
 
-var rootURL = 'http://numbersapi.com';
+  var rootURL = 'http://numbersapi.com';
 
-function generateFact(){
+  function generateFact(){
     var randomNumber = Math.floor(Math.random() * ((200-0)+0) + 0);;
     var xhr = new XMLHttpRequest();
     var factTitle = document.querySelector('.numberFactTitle');
     var fact = document.querySelector('.factString');
     xhr.onload = function(){
-        // do something with the response
-        if (xhr.status === 200) {
-            factTitle.innerHTML = 'Here is a fact about the number ' + randomNumber;
-            fact.innerHTML = xhr.response;
-        }
+      // do something with the response
+      if (xhr.status === 200) {
+        factTitle.innerHTML = 'Here is a fact about the number ' + randomNumber;
+        fact.innerHTML = xhr.response;
+      }
     };
     xhr.open('GET', rootURL + '/' +randomNumber);
     xhr.send();
-}
+  }
 
-document.addEventListener('DOMContentLoaded', generateFact);
+  document.addEventListener('DOMContentLoaded', generateFact);
 
-var factGenerator = document.querySelector('.newNumberFact');
-factGenerator.addEventListener('click', generateFact);
+  var factGenerator = document.querySelector('.newNumberFact');
+  factGenerator.addEventListener('click', generateFact);
+})();
