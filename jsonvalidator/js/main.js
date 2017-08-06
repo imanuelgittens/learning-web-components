fc25d8b0ea488ae9125709359df9febd13254360
Imanuel Gittens <gittensimanuel@gmail.com>
2017-08-06 13:30:41 -0400

    Update JSON validator to ES6.

--- jsonvalidator/js/main.js
+++ jsonvalidator/js/main.js
@@ -1,95 +1,97 @@
-'use strict';
+(function() {
+	'use strict';
 
-var testButton = document.getElementById('runTest');
-var uploadFileButton = document.getElementById('jsonFile');
-var testAgainButton = document.getElementById('testAgain');
+	let testButton = document.getElementById('runTest');
+	let uploadFileButton = document.getElementById('jsonFile');
+	let testAgainButton = document.getElementById('testAgain');
 
-function validateJSON(jsonString) {
-	//var validJSON = /((\[\{)|(,\{)|((\s)+,))((\s|)+"[a-zA-Z0-0]+":"[a-zA-Z0-9_:;\s]+"(\s|)+((,(\s|)+(?="))|((\}(\s|)+((?=,)|\])))))+/;
-	var validJSON = /^{\s*?(\s?"\w*"\s?:((\s?"\w*")|({\s*?(("\w*":"\w*"),?\s*)*\s*?})*),?\s?)*\s*?}$/;
-	return validJSON.test(jsonString);
-}
-
-/*Select the file from the input*/
-function startRead() {
-	var jsonFile = document.getElementById('jsonFile');
-	var jsonFileLabel = document.getElementById('jsonFileLabel');
-	var uploadLabelText = document.getElementById('jsonFileSpan');
-	if (jsonFile) {
-		var filename = jsonFile.files[0].name;
-		uploadLabelText.innerHTML = filename;
-		jsonFileLabel.classList.add('tab-area__actions__file-label--uploaded');
-		convertToString(jsonFile.files[0]);
-	} else {
-		alert('Please upload a file!');
+	function validateJSON(jsonString) {
+		//let validJSON = /((\[\{)|(,\{)|((\s)+,))((\s|)+"[a-zA-Z0-0]+":"[a-zA-Z0-9_:;\s]+"(\s|)+((,(\s|)+(?="))|((\}(\s|)+((?=,)|\])))))+/;
+		let validJSON = /^{\s*?(\s?"\w*"\s?:((\s?"\w*")|({\s*?(("\w*":"\w*"),?\s*)*\s*?})*),?\s?)*\s*?}$/;
+		return validJSON.test(jsonString);
 	}
-}
 
-/*Convert the file to a string*/
-function convertToString(jsonFile) {
-	var reader = new FileReader();
-	reader.readAsText(jsonFile, 'UTF-8');
-	reader.onload = loaded;
-	reader.onerror = errorHandler;
-}
-
-function loaded(evt) {
-	// Obtain the read file data
-	var fileString = evt.target.result;
-	var jsonTextArea = document.getElementById('jsonText');
-	jsonTextArea.value = '';
-	setTimeout(function() {
-		jsonTextArea.value = fileString;
-	}, 1000);
-}
-
-function errorHandler(evt) {
-	if (evt.target.error.name == 'NotReadableError') {
-		alert('File could not be read.');
-	}
-}
-
-// Event Listeners
-
-// Validate the json
-testButton.addEventListener('click', function(event) {
-	var jsonTextArea = document.getElementById('jsonText').value;
-	if (jsonTextArea.length < 1) {
-		alert('Enter some JSON in the textarea.');
-	} else {
-		var tab1 = document.getElementById('jsonstring');
-		var tab2 = document.getElementById('jsonresult');
-		tab1.checked = false;
-		tab2.checked = true;
-		if (validateJSON(jsonTextArea)) {
-			var result = document.getElementById('vResult');
-			var resultText = document.getElementById('vResultText');
-			var resultIcon = document.getElementById('vResultIcon');
-			resultIcon.innerHTML = '<i class="fa fa-smile-o" aria-hidden="true"></i>';
-			resultText.innerHTML = 'Congratulations! Your JSON is Valid!';
+	/*Select the file from the input*/
+	function startRead() {
+		let jsonFile = document.getElementById('jsonFile');
+		let jsonFileLabel = document.getElementById('jsonFileLabel');
+		let uploadLabelText = document.getElementById('jsonFileSpan');
+		if (jsonFile) {
+			let filename = jsonFile.files[0].name;
+			uploadLabelText.innerHTML = filename;
+			jsonFileLabel.classList.add('tab-area__actions__file-label--uploaded');
+			convertToString(jsonFile.files[0]);
 		} else {
-			var result = document.getElementById('vResult');
-			var resultText = document.getElementById('vResultText');
-			var resultIcon = document.getElementById('vResultIcon');
-			resultIcon.innerHTML = '<i class="fa fa-frown-o" aria-hidden="true"></i>';
-			resultText.innerHTML = 'Sorry...something is wrong with your JSON.';
+			alert('Please upload a file!');
 		}
 	}
-});
 
-uploadFileButton.addEventListener('change', function(event) {
-	startRead();
-});
+	/*Convert the file to a string*/
+	function convertToString(jsonFile) {
+		let reader = new FileReader();
+		reader.readAsText(jsonFile, 'UTF-8');
+		reader.onload = loaded;
+		reader.onerror = errorHandler;
+	}
 
-testAgainButton.addEventListener('click', function(event) {
-	var jsonTextArea = document.getElementById('jsonText');
-	var uploadLabelText = document.getElementById('jsonFileSpan');
-	var jsonFileLabel = document.getElementById('jsonFileLabel');
-	jsonTextArea.value = '';
-	uploadLabelText.innerHTML = 'Choose a File';
-	var tab1 = document.getElementById('jsonstring');
-	var tab2 = document.getElementById('jsonresult');
-	jsonFileLabel.classList.remove('tab-area__actions__file-label--uploaded');
-	tab1.checked = true;
-	tab2.checked = false;
-});
+	function loaded(evt) {
+		// Obtain the read file data
+		let fileString = evt.target.result;
+		let jsonTextArea = document.getElementById('jsonText');
+		jsonTextArea.value = '';
+		setTimeout(function() {
+			jsonTextArea.value = fileString;
+		}, 1000);
+	}
+
+	function errorHandler(evt) {
+		if (evt.target.error.name == 'NotReadableError') {
+			alert('File could not be read.');
+		}
+	}
+
+	// Event Listeners
+
+	// Validate the json
+	testButton.addEventListener('click', function(event) {
+		let jsonTextArea = document.getElementById('jsonText').value;
+		if (jsonTextArea.length < 1) {
+			alert('Enter some JSON in the textarea.');
+		} else {
+			let tab1 = document.getElementById('jsonstring');
+			let tab2 = document.getElementById('jsonresult');
+			tab1.checked = false;
+			tab2.checked = true;
+			if (validateJSON(jsonTextArea)) {
+				let result = document.getElementById('vResult');
+				let resultText = document.getElementById('vResultText');
+				let resultIcon = document.getElementById('vResultIcon');
+				resultIcon.innerHTML = '<i class="fa fa-smile-o" aria-hidden="true"></i>';
+				resultText.innerHTML = 'Congratulations! Your JSON is Valid!';
+			} else {
+				let result = document.getElementById('vResult');
+				let resultText = document.getElementById('vResultText');
+				let resultIcon = document.getElementById('vResultIcon');
+				resultIcon.innerHTML = '<i class="fa fa-frown-o" aria-hidden="true"></i>';
+				resultText.innerHTML = 'Sorry...something is wrong with your JSON.';
+			}
+		}
+	});
+
+	uploadFileButton.addEventListener('change', function(event) {
+		startRead();
+	});
+
+	testAgainButton.addEventListener('click', function(event) {
+		let jsonTextArea = document.getElementById('jsonText');
+		let uploadLabelText = document.getElementById('jsonFileSpan');
+		let jsonFileLabel = document.getElementById('jsonFileLabel');
+		jsonTextArea.value = '';
+		uploadLabelText.innerHTML = 'Choose a File';
+		let tab1 = document.getElementById('jsonstring');
+		let tab2 = document.getElementById('jsonresult');
+		jsonFileLabel.classList.remove('tab-area__actions__file-label--uploaded');
+		tab1.checked = true;
+		tab2.checked = false;
+	});
+})();
