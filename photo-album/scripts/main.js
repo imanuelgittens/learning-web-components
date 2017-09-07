f4d27bb9bf48dda1a560bacb7c4821c66c091733
Imanuel Gittens <gittensimanuel@gmail.com>
2017-09-07 12:20:46 -0400

    Redone code style of photo gallery.

--- photo-album/scripts/main.js
+++ photo-album/scripts/main.js
@@ -1,27 +1,48 @@
 (function($, Handlebars) {
-	var $photosContainer = $('#all-photos');
-	var photosURL = 'data/photos.json';
-	//var context = {title: "My New Post", body: "This is my first post!"};
+	'use strict';
 
-	var templateStr = $('#photo-template').html();
-	//console.log(templateStr);
-	var photoTemplate = Handlebars.compile(templateStr);
-
-	var setPhotos = function(photos) {
-		var html = photoTemplate({ photos: photos });
-		console.log(html);
-		$photosContainer.append(html);
-	};
-
-	$.get(photosURL, function(response) {
-		var twentyRandom = [];
-		var i;
-		var random = 0;
-		for(i = 0; i < 20; i++){
-			random = Math.floor(Math.random() * response.length) + 1;
-			twentyRandom.push(response[random]);
-			response.splice(random, 1);
+	$(document).ready(function(){
+		var photoGallery = {
+			photosURL: 'data/photos.json',
+			init: function(){
+				this.cacheDom();
+				this.bindEvents();
+				this.fetchPhotos();
+			},
+			cacheDom: function(){
+				this.$el = $('#all-photos');
+				this.$templateStr = $('#photo-template');
+			},
+			bindEvents: function(){
+				this.$el.on('click', '#single-photo', this.handlePhotoClick.bind(this));
+			},
+			fetchPhotos: function(){
+					var galleryObject = this;
+					$.get(this.photosURL, function(response) {
+						var twentyRandom = [];
+						var i;
+						var random = 0;
+						for(i = 0; i < 20; i++){
+							random = Math.floor(Math.random() * response.length) + 1;
+							twentyRandom.push(response[random]);
+							response.splice(random, 1);
+						}
+						galleryObject.setPhotos(twentyRandom);
+					});
+					
+			},
+			setPhotos: function(photos) {
+				var templateStr = this.$templateStr.html();
+				var photoTemplate = Handlebars.compile(templateStr);
+				var html = photoTemplate({ photos: photos });
+				this.$el.append(html);
+			},
+			handlePhotoClick: function(){
+				console.log('photo clicked');
+			}
 		}
-		setPhotos(twentyRandom);
+
+		photoGallery.init();
 	});
-})(Zepto, Handlebars);
+
+})(jQuery, Handlebars);
