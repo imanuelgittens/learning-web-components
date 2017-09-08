639421b958d85f7688bb28ecb06c56d5dc6bb242
Imanuel Gittens <gittensimanuel@gmail.com>
2017-09-08 06:58:14 -0400

    Implement modal on photo album project.

--- photo-album/scripts/main.js
+++ photo-album/scripts/main.js
@@ -3,6 +3,7 @@
 
 	$(document).ready(function(){
 		var photoGallery = {
+			twentyRandomPhotos: [],
 			photosURL: 'data/photos.json',
 			init: function(){
 				this.cacheDom();
@@ -12,22 +13,22 @@
 			cacheDom: function(){
 				this.$el = $('#all-photos');
 				this.$templateStr = $('#photo-template');
+				this.$galleryModal = $('#gallery-modal');
 			},
 			bindEvents: function(){
-				this.$el.on('click', '#single-photo', this.handlePhotoClick.bind(this));
+				this.$el.on('click', '.photo', this.handlePhotoClick.bind(this));
 			},
 			fetchPhotos: function(){
 					var galleryObject = this;
 					$.get(this.photosURL, function(response) {
-						var twentyRandom = [];
 						var i;
 						var random = 0;
 						for(i = 0; i < 20; i++){
-							random = Math.floor(Math.random() * response.length) + 1;
-							twentyRandom.push(response[random]);
+							random = Math.floor(Math.random() * (response.length - 1)) + 1;
+							galleryObject.twentyRandomPhotos.push(response[random]);
 							response.splice(random, 1);
 						}
-						galleryObject.setPhotos(twentyRandom);
+						galleryObject.setPhotos(galleryObject.twentyRandomPhotos);
 					});
 					
 			},
@@ -37,8 +38,25 @@
 				var html = photoTemplate({ photos: photos });
 				this.$el.append(html);
 			},
-			handlePhotoClick: function(){
-				console.log('photo clicked');
+			handlePhotoClick: function(event){
+				var galleryObject = this;
+				var target = event.target;
+				var photoContainer = target.closest('div');
+				var photoId = $(photoContainer).data('photoid');
+				var photoObject = galleryObject.twentyRandomPhotos.find(function(photo){
+					return photo.id === photoId;
+				});
+				console.log(photoObject);
+				var photoURL = photoObject.url;
+				$(photoContainer).avgrund({
+					showClose: true,
+					showCloseText: 'close',
+					width: 350,
+					height: 350,
+					template: '<img height=350 id="#gallery-modal__img" src="'+photoURL+'"/>'
+				});
+				//activating the modal code doesn't make it appear so we must trigger a click
+				$(photoContainer).trigger('click'); 		
 			}
 		}
 
